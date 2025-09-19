d1486dd435e14ed2858f19eb3363710813c69018
ju5nj0se <juaan.josehernandez@gmail.com>
2025-09-19 15:01:15 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿bool flag = true;
+
+while (flag)
+{
+    Console.Write("-- Riwi Music --\n" +
+                  "1. Administrar conciertos \n" +
+                  "2. Administrar clientes \n" +
+                  "3. Administrar tiquetes \n" +
+                  "4. Historial de compras de clientes \n" +
+                  "5. Consultas avanzadas \n" +
+                  "6. Salir\n" +
+                  ">> ");
+    string option =  Console.ReadLine();
+
+    switch (option)
+    {
+        default:
+            Console.WriteLine("Invalid option!");
+            break;
+    }
+}
